bb5004d5b180b602918a635733ff0f474344bb2d
Edson_Victor <cruz.menezes@outlook.com>
2023-05-05 01:56:44 -0300

    testando

--- api-github/src/pages/MainPage/index.jsx
+++ api-github/src/pages/MainPage/index.jsx
@@ -1,17 +1,24 @@
 import React from "react";
 
-import { Container } from "./styles";
+import { MdSearch } from 'react-icons/md'
+
+import { Container, Logo, Title, Form, Buttom, Input } from './styles';
+
+import githubLogo from '../../assets/images/github-logo.svg'
 
 const MainPage = () => (
 
   <Container>
-    <img
-      src="https://devsamurai-materials.s3.amazonaws.com/templates/avatar-gradient-dark.svg"
-      height="256"
-      alt="Dev Samurai"
-    />
-    <h1>Fala Samurai!</h1>
-    <p>Esse é o template typescript básico da Dev Samurai para React.</p>
+    <Logo src={githubLogo} alt="Api GitHub" />
+    <Title>Api GitHub</Title>
+    <Form>
+      <Input placeholder="Username" />
+      <Button>
+        <MdSearch size={42} />
+      </Button>
+
+    </Form>
+
   </Container>
 );
 
